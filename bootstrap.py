7d6fa1ca361576b350042d00b8d515e82f07e189
root <root@services02.signs365.local>
2015-04-16 15:26:50 -0400

    added centos bootstrapping

---
+++ bootstrap.py
@@ -0,0 +1,60 @@
+from fabric.api import *
+import os, time, logging, re, sys
+from pprint import *
+
+# uncomment for debugging
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+pprint(locals())
+
+epoch_time = int(time.time())
+
+def centos6(ip,hostname):
+        """Update and configure a clean Centos 6.5 machine
+        """
+        env.user = 'root'
+        with settings(host_string='{0}'.format(ip)):
+
+                # disable selinux
+                run('sed -i "s/\=enforcing/\=disabled/g" /etc/selinux/config')
+
+                # initial centos repositories
+                run('yum install -y epel-release')
+
+                # update system
+                run('yum update -y')
+                run('yum remove -y vim-minimal')
+                run('yum groupinstall -y core; yum -y groupinstall base;')
+                run('yum install -y kernel-devel vim wget')
+
+                # custom colors and history format
+                run('sed -i "s/DIR\ 01\;34/DIR\ 40\;33/g" /etc/DIR_COLORS ')
+
+                # temporary disable iptables
+                run('service iptables stop')
+                run('chkconfig iptables off')
+
+                # set hostname
+                run('echo "127.0.0.1 {0} localhost localhost.localdomain localhost4 localhost4.localdomain4" > /etc/hosts'.format(hostname))
+                run('echo "::1 localhost localhost.localdomain localhost6 localhost6.localdomain6" >> /etc/hosts'.format(hostname))
+                run('sed -i "s/HOSTNAME=localhost.localdomain/HOSTNAME={0}/g" /etc/sysconfig/network'.format(hostname))
+
+                # set nameserver and search domain
+                run('echo "search signs365.local" > /etc/resolv.conf')
+                run('echo "nameserver 192.168.55.4" >> /etc/resolv.conf')
+
+                # reboot
+                reboot()
+                print('ATTENTION: bootstrapping is finished.')
+
+def xentools(ip):
+        """Install xentools on a xen-based VM
+        """
+        env.user = 'root'
+        with settings(host_string='{0}'.format(ip)):
+    	# test for xentools cd and install
+        	cd_mounted = run('dmesg | grep xvdd')
+                if cd_mounted.succeeded:
+                        run('mount /dev/xvdd /mnt')
+                        run('yum install -y /mnt/Linux/xe-guest-utilities-6.2.0-1120.i386.rpm /mnt/Linux/xe-guest-utilities-xenstore-6.2.0-1120.i386.rpm')
+		print('ATTENTION: xentools installed')
